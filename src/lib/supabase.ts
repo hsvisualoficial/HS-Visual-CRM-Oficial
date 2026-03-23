10c0e70edc3a271adaeef9f1049faf2345c4a4bd
hsvisualoficial <hsvisual.oficial@gmail.com>
2026-03-23 10:43:11 -0300

    chore: update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ppwaxkfpbnhdrwiholdw.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwd2F4a2ZwYm5oZHJ3aWhvbGR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NzgwOTAsImV4cCI6MjA4OTU1NDA5MH0.adROp-aHoQ0GgkCWEIwHy6ESct-267t7y5UQivLPBws';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://pjozbbdgtngljujcuhxk.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqb3piYmRndG5nbGp1amN1aHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMjE0MTQsImV4cCI6MjA4OTY5NzQxNH0.qGGqbtDJjoyGrXOJNAjlZjZqbj61uhsKvde55fW5mJk';
 
 if (!import.meta.env.VITE_SUPABASE_URL) {
-  console.warn('Supabase URL não encontrada nas variáveis de ambiente! Usando fallback de emergência.');
+  console.warn('Supabase URL não encontrada nas variáveis de ambiente! Usando fallback.');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
